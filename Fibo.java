1e81782ef53b22dd5094908b340c59b25fe97d87
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-03-06 22:32:55 +0530

    update_solve_with_formula

--- Fibo.java
+++ Fibo.java
@@ -1,8 +1,15 @@
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(3));
+        // System.out.println(fibo(3));
+        // for(int i = 0 ; i <= 10 ; i++){
+        //     System.out.println(fiboFormula(i));
+        // }
+        System.out.println(fiboFormula(50));
     }
 
+static int fiboFormula(int n){
+    return (int) ( Math.pow( (1+Math.sqrt(5)) / 2, n ) / Math.sqrt(5) ) ; 
+}
     static int fibo(int n){
         // base condition
         if (n < 2) {
