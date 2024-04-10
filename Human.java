0dbae61b8f6b85ea15488653db41129319edda12
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-04-11 00:02:04 +0530

    cloning

--- Human.java
+++ Human.java
@@ -1,24 +1,34 @@
-public class Human {
+public class Human implements Cloneable{
 
     int age;
     String name;
-    int salary;
-    boolean married;
-    static long population;
+    int[] arr;
 
-    static void message(){
-        System.out.println("hello");
-        // System.out.println(this.age);
-    }
-   
-    public Human(int age , String name , int salary , boolean married ){
-        this.age = age;
+    public Human(int age , String name) {
+        this.age = age ; 
         this.name = name;
-        this.salary = salary;
-        this.married = married;
-        Human.population += 1;
-
-        Human.message();
+        this.arr = new int[]{3,6,1,5,9,7};
     }
-    
+
+    public Human(Human other) {
+        this.age = other.age;
+        this.name = other.name;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+
+        // // this is shallow copy
+        // return super.clone();
+
+        // this is deep copy
+        Human twin = (Human)super.clone();// actually shallow copy
+        // make a deep copy
+        twin.arr = new int[twin.arr.length];
+        for(int i = 0 ; i < twin.arr.length ; i++){
+            twin.arr[i] = this.arr[i];
+        }
+        return twin ;
+        
+    }
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,16 @@
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Student vk = new Student(1, 99.5f);
-        Student kv = new Student(19, 88.8f);
-        Student v = new Student(22, 44.4f);
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Human kajal = new Human(21 , "Kajal chavda");
+        // Human twin = new Human(kajal);
 
-        Student[] list = {vk,kv,v};
-        System.out.println(Arrays.toString(list));
-        Arrays.sort(list , (o1, o2) -> -(int)(o1.marks - o2.marks));
-        System.out.println(Arrays.toString(list));
+        Human twin = (Human)kajal.clone();
+        System.out.println(twin.age +" "+ twin.name);
+        System.out.println(Arrays.toString(twin.arr)); 
 
-        if (vk.compareTo(kv) > 0) {
-            System.out.println(vk.compareTo(kv));
-            System.out.println("vk has more marks");
-        }
+        twin.arr[0] = 100 ;
+        System.out.println(Arrays.toString(kajal.arr));
+        System.out.println(Arrays.toString(twin.arr));
     }
 }
