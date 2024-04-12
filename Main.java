b97643daaab75ae99ed8e0010a93112c7c21821f
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-04-12 20:10:42 +0530

    main for linkedList

--- Main.java
+++ Main.java
@@ -1,27 +1,43 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> list2 = new LinkedList<>();
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(5);
+        // list.insertFirst(29);
+        // list.insertLast(17);
+        // list.insertIndex(8, 2);
+        // list.display();
 
-        list2.add(22);
-        list2.add(44);
-        list2.add(11);
-        list2.add(88);
+        // System.out.println();
+        // System.out.println(list.deleteFirst());
+        // list.display();
 
-        System.out.println(list2);
+        // System.out.println();
+        // System.out.println(list.deleteLast());
+        // list.display();
 
-        List<Integer> vector = new Vector();
+        // System.out.println();
+        // System.out.println(list.deleteIndex(1));
+        // list.display();
 
-        vector.add(22);
-        vector.add(44);
-        vector.add(11);
-        vector.add(88);
+        // System.out.println();
+        // System.out.println(list.deleteIndex(1));
+        // list.display();
 
-        System.out.println(vector);
+        // System.out.println();
+        // System.out.println(list.find(3));
+
+
+
+        DoublyLL list1 = new DoublyLL();
+        list1.insertFirst(3);
+        list1.insertFirst(5);
+        list1.insertFirst(29);
+        list1.insertFirst(27);
+
+        list1.display();
+       
+
+
     }
 }
