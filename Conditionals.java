8f52ca65f78a0415a2d93877f789a93155c3f80b
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-04-24 22:23:20 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,30 @@
+public class Conditionals {
+    public static void main(String[] args) {
+
+        int salary = 25000;
+
+        /*
+         * if (salary > 10000) {
+         * salary = salary + 2000;
+         * } else {
+         * salary = salary + 1000;
+         * }
+         * 
+         * System.out.println(salary);
+         */
+
+        // MULTIPLE IF-ELSE STATEMENT
+
+        if (salary > 10000) {
+            salary += 2000;
+        } else if (salary > 20000) {
+            salary += 3000;
+        } else {
+            salary += 1000;
+        }
+
+        System.out.println(salary);
+
+
+    }
+}
