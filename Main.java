bcbef7e4b0c992575db50ba976060fa5d6609d34
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-04-30 20:43:12 +0530

    AVL main

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // BinaryTree tree = new BinaryTree();
-        // tree.populate(scanner);
-        // tree.preetyDisplay();
 
-        BST tree = new BST();
-        int nums[] = {1,2,3,4,5,6,7,8};
-        tree.populate(nums);
-        // tree.populatedSorted(nums);
-        tree.display();
+class Main {
+    public static void main(String[] args) {
+      AVL tree = new AVL();
+  
+      for(int i=0; i < 1000; i++) {
+        tree.insert(i);
+      }
+  
+      System.out.println(tree.height());
     }
-}
+  }
