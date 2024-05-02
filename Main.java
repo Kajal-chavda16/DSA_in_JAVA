89c91e64f4dafb02c3c78abf8687b3f9facd85c3
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-05-02 23:05:05 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,66 +1,54 @@
-import java.math.BigInteger;
-import java.math.BigDecimal;
+import java.util.Random;
+import java.util.Arrays;
+import java.text.DecimalFormat;
 
 class Main {
   public static void main(String[] args) {
-    BD();
+    // constructor 1
+    StringBuffer sb = new StringBuffer();
+    System.out.println(sb.capacity());
+
+    // constructor 2
+    StringBuffer sb2 = new StringBuffer("Kunal Kushwaha");
+
+    // constructor 3
+    StringBuffer sb3 = new StringBuffer(30);
+    System.out.println(sb3.capacity());
+
+
+    sb.append("WeMakeDevs");
+    sb.append(" is nice!");
+
+    // sb.insert(2, " Rahul ");
+
+    sb.replace(1, 5, "Kushwaha");
+
+    sb.delete(1, 5);
+
+    // sb.reverse();
+
+    String str = sb.toString();
+    System.out.println(str);
+
+    int n = 20;
+    String name = RandomString.generate(n);
+    System.out.println(name);
+
+    // remove whitespaces
+
+    String sentence = "Hi h   hjh  hjkso  siowi     w ";
+    System.out.println(sentence);
+
+    System.out.println(sentence.replaceAll("\\s", ""));
+
+    // split
+
+    String arr = "Kunal,Apoorv,Rahul,Snehal";
+    String[] names = arr.split(",");
+    System.out.println(Arrays.toString(names));
+
+    // rounding off
+    DecimalFormat df = new DecimalFormat("00.0000");
+    System.out.println(df.format(7.29));
   }
-
-  static void BD() {
-    double x = 0.03;
-    double y= 0.04;
-    // double ans = y-x;
-    // System.out.println(ans);
-
-    BigDecimal X = new BigDecimal("0.03");
-    BigDecimal Y = new BigDecimal("0.04");
-    BigDecimal ans = Y.subtract(X);
-    // System.out.println(ans);
-
-    BigDecimal a = new BigDecimal("456576345675.4546376");
-    BigDecimal b = new BigDecimal("547634565352.986785764");
-
-    // operations
-    System.out.println(b.add(a));
-    System.out.println(b.subtract(a));
-    System.out.println(b.multiply(a));
-    System.out.println(b.pow(2));
-    System.out.println(b.negate());
-
-    // constants
-    BigDecimal con = BigDecimal.ONE;
-
-  }
-
-  
-  static void BI() {
-    int a = 30;
-    int b = 67;
-
-    BigInteger B = BigInteger.valueOf(6); // convert int/long to BI
-    int c = B.intValue(); // convert BI to int
-    BigInteger C = new BigInteger("2345678654325678976543256789");
-    BigInteger X = new BigInteger("4536789765432");
-
-    // constants
-    BigInteger D = BigInteger.TEN;
-
-    // operations
-    BigInteger s = C.add(X);
-
-    BigInteger m = C.multiply(X);
-
-    BigInteger sub = C.subtract(X);
-
-    BigInteger d = C.divide(X);
-
-    BigInteger rem = C.remainder(X);
-
-    if (X.compareTo(C) < 0) {
-          System.out.println("Yes");
-    } 
-
-    System.out.println(Factorial.fact(100));
-  }
-  
 }
