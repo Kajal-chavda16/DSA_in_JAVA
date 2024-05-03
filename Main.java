1de0a5d23b1018648f62074b7f740197205e1405
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-05-03 11:44:34 +0530

    Main_for_hashmap

--- Main.java
+++ Main.java
@@ -1,54 +1,89 @@
-import java.util.Random;
-import java.util.Arrays;
-import java.text.DecimalFormat;
+
+import java.util.*;
+
+class MapUsingHash {
+
+  private Entity[] entities;
+  
+  public MapUsingHash() {
+    entities = new Entity[100];
+  }
+
+  public void put(String key, String value) {
+    int hash = Math.abs(key.hashCode() % entities.length);
+    entities[hash] = new Entity(key, value); // overriding
+  }
+
+  public String get(String key) {
+    int hash = Math.abs(key.hashCode() % entities.length);
+    if(entities[hash] != null && entities[hash].key.equals(key)) {
+      return entities[hash].value;
+    }
+    return null;
+  }
+
+  public void remove(String key) {
+    int hash = Math.abs(key.hashCode() % entities.length);
+    if(entities[hash] != null && entities[hash].key.equals(key)) {
+    entities[hash] = null;
+    }
+  }
+
+  private class Entity {
+    String key;
+    String value;
+
+    public Entity(String key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+  }
+}
+
+
 
 class Main {
   public static void main(String[] args) {
-    // constructor 1
-    StringBuffer sb = new StringBuffer();
-    System.out.println(sb.capacity());
+    // hashDemo();
+    HashMapFinal<String, String> map = new HashMapFinal<>();
+    
+    map.put("Mango", "King of fruits");
+    map.put("Apple", "A sweet red fruit");
+    map.put("Litchi", "Kunal's fav fruit");
 
-    // constructor 2
-    StringBuffer sb2 = new StringBuffer("Kunal Kushwaha");
-
-    // constructor 3
-    StringBuffer sb3 = new StringBuffer(30);
-    System.out.println(sb3.capacity());
-
-
-    sb.append("WeMakeDevs");
-    sb.append(" is nice!");
-
-    // sb.insert(2, " Rahul ");
-
-    sb.replace(1, 5, "Kushwaha");
-
-    sb.delete(1, 5);
-
-    // sb.reverse();
-
-    String str = sb.toString();
-    System.out.println(str);
-
-    int n = 20;
-    String name = RandomString.generate(n);
-    System.out.println(name);
-
-    // remove whitespaces
-
-    String sentence = "Hi h   hjh  hjkso  siowi     w ";
-    System.out.println(sentence);
-
-    System.out.println(sentence.replaceAll("\\s", ""));
-
-    // split
-
-    String arr = "Kunal,Apoorv,Rahul,Snehal";
-    String[] names = arr.split(",");
-    System.out.println(Arrays.toString(names));
-
-    // rounding off
-    DecimalFormat df = new DecimalFormat("00.0000");
-    System.out.println(df.format(7.29));
+    System.out.println(map);
+    
+    
   }
+
+  public static void hashDemo() {
+    // String name = "Rahul";
+
+    // Integer a = 4235678;
+
+    // int code = a.hashCode();
+
+    // System.out.println(code);
+
+    HashMap<String, Integer> map = new HashMap<>();
+
+    map.put("Kunal", 89);
+    map.put("Karan", 99);
+    map.put("Rahul", 94);
+
+    // System.out.println(map.get("Karan"));
+    // System.out.println(map.getOrDefault("Apoorv", 78));
+    System.out.println(map.containsKey("Karan"));
+
+    HashSet<Integer> set = new HashSet<>();
+    set.add(56);
+    set.add(9);
+    set.add(12);
+    set.add(43);
+    set.add(56);
+    set.add(2);
+
+    System.out.println(set);
+  }
+  
 }
